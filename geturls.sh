670c5e3478d3cdd35975f8977f7506176a556790
Dan Hill <dhill@witsquash.com>
2019-05-28 15:15:23 -0400

    initial commit

---
+++ geturls.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# I use this to get a list of links viewable on the current window 
+#in gnu screen.  
+#I add the following to my .screenrc:
+#
+##linkscan
+#bindkey "^[u" eval 'hardcopy .screen_hardcopy.txt' split 'focus down' 'screen -t links geturls.sh'
+
+#for now just finding 0x4d's we'll find a better regular expression 
+#in a minute
+url_list=$(grep -o http[s?]://[A-Za-z0-9/\.%~_-]* ~/.screen_hardcopy.txt)
+
+count=0
+for url in $url_list
+do
+	let count=$count+1
+	echo " $count) $url"
+done
+count=0
+
+read -p "Make your link selection:" link_no
+
+for url in $url_list
+do
+	let count=$count+1
+	if [ $count == $link_no ]
+		then
+			firefox $url
+	fi
+done
+
+screen -S main -X remove 
+
+
